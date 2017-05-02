5f512bb0ef3e40f1f98ebc5941c90d5f092b13c0
Allison Northrop <agnorthrop@gmail.com>
2017-05-02 16:28:19 -0700

    changed base url

--- lib/edamam_search.rb
+++ lib/edamam_search.rb
@@ -1,18 +1,23 @@
 require 'HTTParty'
 class EdamamSearch
 
-  BASE_URL = "https://api.edamam.com/search"
+  BASE_URL = 'https://api.edamam.com/search?app_id=0da2202a&app_key=6742a524d0f9cb891fa96ab03339a940'
 
-  attr_reader :search
+  # attr_reader :search
+  #
+  # def initialize(search)
+  #   @search = search
+  # end
 
-  def initialize(search)
-    @search = search
+  def self.all_recipes(search)
+    url ="#{BASE_URL}/token=#{ENV["EDAMAM_TOKEN"]}"
+    response = HTTParty.get(url).parsed_response
+
+
+
   end
 
-  # def self.alls
-  #   url =
-  #   response = HTTParty.get(url).parsed_response
-  # end
+
 
 
 
