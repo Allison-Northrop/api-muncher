537ae78f46269438d78cf1acf14e96fecda95567
Allison Northrop <agnorthrop@gmail.com>
2017-05-02 14:42:45 -0700

    initial set + a few other things complete

---
+++ lib/edamam_search.rb
@@ -0,0 +1,18 @@
+class EdamamSearch
+
+  BASE_URL = "https://api.edamam.com"
+
+  attr_reader :search
+
+  def initialize(search)
+    @search = search
+  end
+
+  # def self.alls
+  #   url =
+  #   response = HTTParty.get(url).parsed_response
+  # end
+
+
+
+end
---
+++ test/lib/edamam_search_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+describe EdamamSearch do
+
+  describe "initialize" do
+    it "takes a search"
+      search = "hummus test search"
+      edamam_search = EdamamSearch.new(search)
+      edamam_search.search.must_equal search 
+    end
+
+    it "requires a search before searching" do
+    end
+  end
